492280f18c8d43b3aea5a9e2b671bd43a08f79f0
Pavlo Hrabovets <pgrabovets3056@gmail.com>
2026-03-10 15:09:24 +0200

    add different variant to the App page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,53 @@ import { DatePicker } from "./datepicker/DatePicker";
 function App() {
   return (
     <>
-      <DatePicker isRange={true} hasInputs={true} hasTime={true} />
+      <section>
+        <div>
+          <h1>Default</h1>
+          <p>Component with default config</p>
+          <div>
+            <DatePicker />
+          </div>
+        </div>
+        <div>
+          <h1>Single date</h1>
+          <p>Component with date input</p>
+          <div>
+            <DatePicker hasInputs />
+          </div>
+        </div>
+        <div>
+          <h1>Single date time</h1>
+          <p>Component with date and time inputs</p>
+          <div>
+            <DatePicker hasInputs hasTime />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h1>Range</h1>
+          <p>Component with range</p>
+          <div>
+            <DatePicker isRange />
+          </div>
+        </div>
+        <div>
+          <h1>Range date</h1>
+          <p>Component with date inputs</p>
+          <div>
+            <DatePicker isRange hasInputs />
+          </div>
+        </div>
+        <div>
+          <h1>Range date time</h1>
+          <p>Component with date and time inputs</p>
+          <div>
+            <DatePicker isRange hasInputs hasTime />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
